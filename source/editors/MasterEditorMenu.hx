69321a921a6fccfc33f5c1aaa270f55a4f2324cb
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-25 06:45:52 -0500

    Update MasterEditorMenu.hx

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -26,7 +26,8 @@ class MasterEditorMenu extends MusicBeatState
 		'Dialogue Editor',
 		'Dialogue Portrait Editor',
 		'Character Editor',
-		'Chart Editor'
+		'Chart Editor',
+		'Chart Editor 2'
 	];
 	private var grpTexts:FlxTypedGroup<Alphabet>;
 	private var directories:Array<String> = [null];
@@ -126,6 +127,8 @@ class MasterEditorMenu extends MusicBeatState
 					LoadingState.loadAndSwitchState(new DialogueEditorState(), false);
 				case 'Chart Editor'://felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
+                                case 'Chart Editor 2'://felt it would be cool maybe
+					LoadingState.loadAndSwitchState(new ChartingState2(), false);
 			}
 			FlxG.sound.music.volume = 0;
 			#if PRELOAD_ALL
